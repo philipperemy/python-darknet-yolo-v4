9046ea690abce5b0d465eb1a494dd3d3bbaf111a
Philippe Remy <premy.enseirb@gmail.com>
2020-07-25 16:59:37 +0900

    minor

---
+++ test.py
@@ -0,0 +1,45 @@
+from threading import Thread
+from time import time
+
+from tqdm import tqdm
+
+from yolov4 import Detector, MultiGPU
+
+c = 0
+
+
+def target(g, desc):
+    global c
+    for _ in range(1000):
+        # print(desc)
+        g.perform_detect(show_image=False)
+        if c % 100 == 0:
+            print(time(), c)
+        c += 1
+    print(time(), c)
+
+
+def main_single():
+    d = Detector(gpu_id=0)
+    for _ in tqdm(range(900)):
+        d.perform_detect(show_image=False)
+
+
+def main():
+    g = MultiGPU([
+        Detector(gpu_id=0),
+        Detector(gpu_id=1)
+    ])
+
+    thread1 = Thread(target=target, args=(g, 't1'))
+    thread2 = Thread(target=target, args=(g, 't2'))
+
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
+
+
+if __name__ == '__main__':
+    main()
---
+++ yolov4/__init__.py
@@ -0,0 +1,1 @@
+from yolov4.darknet import Detector, MultiGPU
