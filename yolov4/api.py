8bbcfff0d3c4ce05b30161ce389f62935de0421a
Philippe Remy <premy.enseirb@gmail.com>
2020-07-25 13:36:52 +0900

    works well. 19FPS (not too bad) GPU 80%

--- yolov4/api.py
+++ yolov4/api.py
@@ -3,11 +3,10 @@ import attr
 
 @attr.s
 class DarkNetPredictionResult:
-    # https://www.ccoderun.ca/programming/2019-08-18_Darknet_training_images/
-    class_name = attr.ib(type=str)
-    class_confidence = attr.ib(type=float)
+    class_name = attr.ib(type=str)  # name of the class detected. E.g. dog.
+    class_confidence = attr.ib(type=float)  # probability of the class. E.g. 95%.
     left_x = attr.ib(type=int)  # box center x.
     top_y = attr.ib(type=int)  # box center y.
     width = attr.ib(type=int)  # box width.
     height = attr.ib(type=int)  # box height.
-    info = attr.ib(type=dict, default={})  # extra info about filename...
+    info = attr.ib(type=dict, default={})  # extra info. Can be blank.
